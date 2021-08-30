286ae8d1154c6e50d538c95dca60703184bc967b
Tom Byrne <tom.byrne@imagination.com>
2021-08-30 16:47:14 +1000

    Added NavigationMenu

--- size_checker.Shared/MainPage.xaml.cs
+++ size_checker.Shared/MainPage.xaml.cs
@@ -1,17 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using System.IO;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +14,92 @@ namespace size_checker
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        public ObservableCollection<Source> Sources { get; set; }
+        public ObservableCollection<NavigationViewItemBase> NavItems { get; set; }
+
+        ApplicationDataCompositeValue SourcesData;
+
+        String SourceDataKey = "SourceData";
+
         public MainPage()
         {
             this.InitializeComponent();
+            NavItems = new ObservableCollection<NavigationViewItemBase>();
+            this.SearchForSources();
+        }
+
+        void SearchForSources()
+        {
+
+            Sources = new ObservableCollection<Source>();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (!drive.IsReady) continue;
+
+                String Name = drive.Name + " " + (drive.IsReady ? drive.VolumeLabel : "<Unavailable>");
+                Symbol Glyph;
+                if (!drive.IsReady)
+                {
+                    Glyph = Symbol.DisconnectDrive;
+                }
+                else
+                {
+                    switch (drive.DriveType)
+                    {
+                        case DriveType.Network:
+                            Glyph = Symbol.World;
+                            break;
+
+                        default:
+                            Glyph = Symbol.MapDrive;
+                            break;
+                    }
+                }
+                Sources.Add(new Source { Name = Name, Glyph = Glyph, Tooltip = Name, IsEnabled = drive.IsReady });
+            }
+
+            GenerateNavItems();
+        }
+
+        async void OnAddButtonClick(object sender, ItemClickEventArgs e)
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.ComputerFolder;
+            folderPicker.FileTypeFilter.Add("*");
+            StorageFolder pickedFolder = await folderPicker.PickSingleFolderAsync();
+            if (pickedFolder != null)
+            {
+                Console.WriteLine(pickedFolder.DisplayName);
+                Sources.Add(new Source { Name = pickedFolder.Path, Glyph = Symbol.Folder, Tooltip = pickedFolder.Path, IsEnabled = true });
+                GenerateNavItems();
+            }
+            else
+            {
+                Console.WriteLine("Add Source aborted");
+            }
+
+        }
+
+        void GenerateNavItems()
+        {
+            NavItems.Clear();
+
+            NavItems.Add(new NavigationViewItemHeader { Content = "Sources"});
+
+            foreach (Source Source in Sources)
+            {
+                NavItems.Add(new NavigationViewItem
+                {
+                    Content = Source.Name,
+                    Icon = new SymbolIcon(Source.Glyph),
+                    IsEnabled = Source.IsEnabled,
+                });
+            }
+
         }
     }
+
 }
---
+++ size_checker.Shared/Source.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace size_checker
+{
+
+    public class Source
+    {
+        public string Name { get; set; }
+        public string Tooltip { get; set; }
+        public Symbol Glyph { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
